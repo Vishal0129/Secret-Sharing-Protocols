12fc80e3555fa4c437eb993988e2c9c4f8625e5a
Vishal0129 <vishalsai010304@gmail.com>
2023-07-29 20:45:56 +0530

    vss using fields

--- VSS.py
+++ VSS.py
@@ -25,7 +25,25 @@ def mod_inverse(a, m):
 def field_prime_value(num, p):
     return num % p
 
+# def validate(index, value, commitments, g, p, p1):
+#     val_value = pow(g, value, p1)
+#     product = 1
+#     for i, j in enumerate(commitments):
+#         product = (product * pow(j, index ** i)%p1) % p1
+#     print(val_value, product)
+#     return val_value == product
+def validate(index, value, commitments, g, p, p1):
+    val_value = pow(g, value, p1)
+    product = 1
 
+    for i, j in enumerate(commitments):
+        product = (product * pow(j, index ** i, p1)) % p1
+
+    val_value = field_prime_value(val_value, p1)
+    product = field_prime_value(product, p1)
+
+    print(val_value, product)
+    return val_value == product
 
 if __name__ == "__main__":
     url_valid = 'https://hash-effect.onrender.com/vss/share/valid'
